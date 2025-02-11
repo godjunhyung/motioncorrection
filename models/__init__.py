5ef5f41af9e5f7c7d50a305464445c05596c7b33
junsenkim <jjunkaai@gmail.com>
2025-02-12 07:57:40 +0900

    .

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .resnet import SupConResNet, SupRegResNet, SupCEResNet, LinearRegressor
-from .alexnet import SupConAlexNet, SupRegAlexNet
-from .densenet import SupConDenseNet, SupRegDenseNet
-from .estimators import AgeEstimator, SiteEstimator
